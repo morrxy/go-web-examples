6cd59c28f38bd1105bf7f151e87f13f801016d70
morrxy <morrxy@gmail.com>
2017-04-16 22:40:39 +0800

    first commit

---
+++ 02.5/02.5.3/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const (
+	WHITE = "iota"
+	BLACK
+	BLUE
+	RED
+	YELLOW
+)
+
+type Color byte
+
+type Box struct {
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+go web 编程例子
+
+go web 编程：https://github.com/astaxie/build-web-application-with-golang
